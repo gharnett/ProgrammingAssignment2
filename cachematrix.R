bd7d1a74b0befde05dd1171bd586035afcb0b7d4
gharnett <gharnett@q.com>
2014-10-25 18:55:09 -0600

    cache or update a matrix and its inverse
    The use of “super-assignment” ( <<-) in R is illustrated with an object
    that
    enables caching the inverse and updating the matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The pair of functions here enable the caching of a matrix and its inverse in one object.
+## The inverse is not calculated until first requested; subsequent requests return the cached value.
+## The object admits a method to reset the matrix held by the object to a new value.
+## After resetting, the inverse is again not calculated until requested.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# The function makeCacheMatrix takes a presumed-invertible matrix and
+# returns a list of four items.
+# Two of the items are empty-argument functions that return the current values of the matrix
+# and its inverse (which latter is initially null).
+# Two of the items are functions which enable the reset of the values of the matrix and its inverse.
+makeCacheMatrix <- function(m = matrix()) {
+	
+    # The value of minv is to be the inverse of the current value of variable m,
+    # which takes values of an invertible square matrix.
+    # It's what x$currentInverse() will return if x is a value of makeCacheMatrix.
+    # Also this establishes minv as a member of the environment associated with x.
+    minv <- NULL
+    
+    resetMatrixF <- function(newm) {
+        # When resetMatrixF is called by x$resetMatrix(arg),
+        # the assignment syntax <-- imposes a lookup
+        #--outside the resetMatrixF environment--for the "home" of variable m,
+        # which will be the environment associated with x.
+        m <<- newm
+        minv <<- NULL
+    }
+    
+    # setInverseF should be called with the inverse of the current value of the matrix m
+    # the inverse being computed elsewhere.
+    setInverseF <- function(shouldBeInverse) minv <<- shouldBeInverse
+    matrixValueF <- function() m
+    inverseValueF <- function() minv
+    
+    # The returned list, say x, has the following four elements.
+    # The first two reference the functions that reset values in the environment of x.
+    # The second two reference the constant functions whose
+    # values are respectively the matrix and its inverse (possibly null).
+    list(
+	resetMatrix = resetMatrixF,
+	setInverse = setInverseF,
+	currentMatrix = matrixValueF,
+	currentInverse = inverseValueF
+    )
 }
 
-
-## Write a short comment describing this function
-
+## The function cacheSolve takes a value x of makeCacheMatrix
+## and returns an ordinary matrix---the inverse of the matrix carried by x, namely x$currentMatrix().
+## If cacheSolve has been previously called on x carrying the same internal values,
+## then it simply returns the cached value of the inverse.
+## Otherwise, it calculates the inverse, caches it, as well as returning it.
+## So there are side effects of this thing!
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    xinv <- x$currentInverse()
+    if(!is.null(xinv)) {
+        message("getting cached inverse")
+        return(xinv)
+    }
+    # otherwise
+    xinv <- solve(x$currentMatrix())   # Request the matrix itself and calculate its inverse.
+    x$setInverse(xinv)  # Ask x to set its currentInverse value to be xinv.
+    xinv  # BTW return the inverse that was calculated.
 }
